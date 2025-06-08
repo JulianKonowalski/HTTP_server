33f5727c5863baaaf087e5f7cd7f08eb61bfe579
JulianKonowalski <julian.konwalski@gmail.com>
2025-06-08 16:56:57 +0200

    implement http request and response classes

--- include/HTTP/HTTPResponse.h
+++ include/HTTP/HTTPResponse.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <cstdint>
 #include <unordered_map>
 
 namespace server::http {
@@ -8,8 +9,35 @@ namespace server::http {
 class HTTPResponse {
 public:
 
+    using Headers = std::unordered_map<std::string, std::string>;
+
+    HTTPResponse(
+        const std::string& httpVersion,
+        const uint16_t& responseCode,
+        const std::string& reasonPhrase,
+        const Headers& headers,
+        const std::string& body 
+    ) : mHttpVersion(httpVersion),
+        mResponseCode(responseCode),
+        mReasonPhrase(reasonPhrase),
+        mHeaders(headers),
+        mBody(body)
+    {}
+
+    const std::string& get_http_version(void) { return mHttpVersion; }
+    const uint16_t& get_response_code(void) { return mResponseCode; }
+    const std::string& get_reason_phrase(void) { return mReasonPhrase; }
+    const Headers& get_headers(void) { return mHeaders; }
+    const std::string& get_body(void) { return mBody; }
+
 private:
-    std::unordered_map<std::string, std::string> mData;
+
+    const std::string mHttpVersion;
+    const uint16_t mResponseCode;
+    const std::string mReasonPhrase;
+    const Headers mHeaders;
+    const std::string mBody;
+
 };
 
 }
--- src/HTTP/HTTPRequest.cpp
+++
@@ -1,1 +0,0 @@
-#include "HTTP/HTTPRequest.h"
